044191e8b5c53ba63482e204e32e04bb387107cd
Artem <lazarazuy@gmail.com>
2022-01-06 13:27:14 +0200

    +

--- src/App.js
+++ src/App.js
@@ -22,7 +22,9 @@ export class App extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
-    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
     // console.log("didMount", this.state.contacts);
   }
   componentDidUpdate(prevProps, prevState) {
